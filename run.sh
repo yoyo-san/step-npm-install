605cd89dbd8b5d4f45d6e233afb5ad601ffd0ad8
pjvds <pj@born2code.net>
2013-10-28 16:40:57 +0100

    Fix wrong directory creation

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mkdir -p $WERCKER_CACHE_DIR/wercker/npm
-npm config set cache $WERCKER_CACHE_DIR/cache/wercker/npm
+npm config set cache $WERCKER_CACHE_DIR/wercker/npm
 npm install $WERCKER_NPM_INSTALL_OPTIONS
